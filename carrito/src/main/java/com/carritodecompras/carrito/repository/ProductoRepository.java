7ef2ffde476b0263042cfda921123cc8b0adda45
SheyAri30 <sheylaarizandieta3@gmail.com>
2025-10-31 19:55:02 -0600

    Delete carrito directory

--- carrito/src/main/java/com/carritodecompras/carrito/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carritodecompras.carrito.repository;
-
-import com.carritodecompras.carrito.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
